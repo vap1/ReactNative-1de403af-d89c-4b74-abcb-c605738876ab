6d8cf3a1de549ff7c71784b953d0af86ed9755ae
vap1 <vaibhavvasant1705@gmail.com>
2023-08-25 15:22:57 +0530

    First Commit

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -2,9 +2,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import HomeScreen from '../screens/HomeScreen';
-import RegistrationScreen from '../screens/RegistrationScreen';
 import EmployeeManagementScreen from '../screens/EmployeeManagementScreen';
 import LeadManagementScreen from '../screens/LeadManagementScreen';
 import OpportunityTrackingScreen from '../screens/OpportunityTrackingScreen';
@@ -17,7 +15,6 @@ const AppNavigation: React.FC = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Registration" component={RegistrationScreen} />
         <Stack.Screen name="EmployeeManagement" component={EmployeeManagementScreen} />
         <Stack.Screen name="LeadManagement" component={LeadManagementScreen} />
         <Stack.Screen name="OpportunityTracking" component={OpportunityTrackingScreen} />
