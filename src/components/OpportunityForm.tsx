3f210b11aa84dc510485bf846c60f6e5b212c22e
vap1 <vaibhavvasant1705@gmail.com>
2023-08-25 15:23:04 +0530

    First Commit

--- src/components/OpportunityForm.tsx
+++ src/components/OpportunityForm.tsx
@@ -1,50 +1,109 @@
 
 import React, { useState } from 'react';
-import { View, TextInput, Button } from 'react-native';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { OpportunityRequest } from '../types/OpportunityTypes';
+import { createOpportunity } from '../apis/OpportunityApi';
 
 interface OpportunityFormProps {
-  onSubmit: (opportunity: OpportunityDetails) => void;
-}
-
-interface OpportunityDetails {
-  // Define the fields for opportunity details here
+  onSubmit: () => void;
 }
 
 const OpportunityForm: React.FC<OpportunityFormProps> = ({ onSubmit }) => {
-  const [opportunity, setOpportunity] = useState<OpportunityDetails>({
-    // Initialize the opportunity details state here
-  });
+  const [leadId, setLeadId] = useState('');
+  const [assignedTo, setAssignedTo] = useState('');
+  const [status, setStatus] = useState('');
+  const [notes, setNotes] = useState('');
+  const [documents, setDocuments] = useState('');
 
-  const handleInputChange = (field: keyof OpportunityDetails, value: string) => {
-    setOpportunity((prevOpportunity) => ({
-      ...prevOpportunity,
-      [field]: value,
-    }));
+  const handleLeadIdChange = (text: string) => {
+    setLeadId(text);
   };
 
-  const handleSubmit = () => {
-    onSubmit(opportunity);
+  const handleAssignedToChange = (text: string) => {
+    setAssignedTo(text);
+  };
+
+  const handleStatusChange = (text: string) => {
+    setStatus(text);
+  };
+
+  const handleNotesChange = (text: string) => {
+    setNotes(text);
+  };
+
+  const handleDocumentsChange = (text: string) => {
+    setDocuments(text);
+  };
+
+  const handleSubmit = async () => {
+    const opportunityRequest: OpportunityRequest = {
+      leadId,
+      assignedTo,
+      status,
+      notes,
+      documents,
+    };
+
+    try {
+      await createOpportunity(opportunityRequest);
+      onSubmit();
+    } catch (error) {
+      console.error('Error creating opportunity:', error);
+    }
   };
 
   return (
-    <View>
-      {/* Render the input fields for opportunity details */}
+    <View style={styles.container}>
       <TextInput
-        value={opportunity.field1}
-        onChangeText={(value) => handleInputChange('field1', value)}
-        placeholder="Field 1"
+        style={styles.input}
+        placeholder="Lead ID"
+        value={leadId}
+        onChangeText={handleLeadIdChange}
       />
       <TextInput
-        value={opportunity.field2}
-        onChangeText={(value) => handleInputChange('field2', value)}
-        placeholder="Field 2"
+        style={styles.input}
+        placeholder="Assigned To"
+        value={assignedTo}
+        onChangeText={handleAssignedToChange}
       />
-      {/* Add more input fields for other opportunity details */}
-      
-      {/* Render the submit button */}
+      <TextInput
+        style={styles.input}
+        placeholder="Status"
+        value={status}
+        onChangeText={handleStatusChange}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Notes"
+        value={notes}
+        onChangeText={handleNotesChange}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Documents"
+        value={documents}
+        onChangeText={handleDocumentsChange}
+      />
       <Button title="Submit" onPress={handleSubmit} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+});
 
 export default OpportunityForm;
