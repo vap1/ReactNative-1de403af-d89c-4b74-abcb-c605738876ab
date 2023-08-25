6d8fc32871ae1a72a512b0de8eff05fa6c5b8776
vap1 <vaibhavvasant1705@gmail.com>
2023-08-25 15:23:03 +0530

    First Commit

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,51 +1,84 @@
 
 import React, { useState } from 'react';
-import { View, TextInput, Button, Alert } from 'react-native';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
 
 interface RegistrationFormProps {
-  onRegister: (username: string, password: string, email: string, role: string) => void;
+  onSubmit: (username: string, password: string, email: string, role: string) => void;
 }
 
-const RegistrationForm: React.FC<RegistrationFormProps> = ({ onRegister }) => {
+const RegistrationForm: React.FC<RegistrationFormProps> = ({ onSubmit }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [role, setRole] = useState('');
 
-  const handleRegister = () => {
-    if (username && password && email && role) {
-      onRegister(username, password, email, role);
-    } else {
-      Alert.alert('Error', 'Please fill in all fields');
-    }
+  const handleUsernameChange = (text: string) => {
+    setUsername(text);
+  };
+
+  const handlePasswordChange = (text: string) => {
+    setPassword(text);
+  };
+
+  const handleEmailChange = (text: string) => {
+    setEmail(text);
+  };
+
+  const handleRoleChange = (text: string) => {
+    setRole(text);
+  };
+
+  const handleSubmit = () => {
+    onSubmit(username, password, email, role);
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput
+        style={styles.input}
         placeholder="Username"
         value={username}
-        onChangeText={setUsername}
+        onChangeText={handleUsernameChange}
       />
       <TextInput
+        style={styles.input}
         placeholder="Password"
         secureTextEntry
         value={password}
-        onChangeText={setPassword}
+        onChangeText={handlePasswordChange}
       />
       <TextInput
+        style={styles.input}
         placeholder="Email"
         value={email}
-        onChangeText={setEmail}
+        onChangeText={handleEmailChange}
       />
       <TextInput
+        style={styles.input}
         placeholder="Role"
         value={role}
-        onChangeText={setRole}
+        onChangeText={handleRoleChange}
       />
-      <Button title="Register" onPress={handleRegister} />
+      <Button title="Submit" onPress={handleSubmit} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+});
 
 export default RegistrationForm;
