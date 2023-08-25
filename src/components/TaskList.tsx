138cb634082e2f30499839a518df67974cf6f1e1
vap1 <vaibhavvasant1705@gmail.com>
2023-08-25 15:23:08 +0530

    First Commit

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,44 +1,67 @@
 
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
-import { TaskDetails } from '../api/types'; // Import the API types
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { Task } from '../types/TaskTypes';
+import { getTasks } from '../apis/TaskApi';
 
 const TaskList: React.FC = () => {
-  const [tasks, setTasks] = useState<TaskDetails[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   useEffect(() => {
-    // Fetch tasks from the API
-    const fetchTasks = async () => {
-      try {
-        const response = await fetch('/tasks');
-        const data = await response.json();
-        setTasks(data);
-      } catch (error) {
-        console.error('Error fetching tasks:', error);
-      }
-    };
-
     fetchTasks();
   }, []);
 
-  const renderTaskItem = ({ item }: { item: TaskDetails }) => (
-    <View>
-      <Text>{item.taskName}</Text>
-      <Text>{item.status}</Text>
-      {/* Render other task details */}
+  const fetchTasks = async () => {
+    try {
+      const response = await getTasks();
+      setTasks(response);
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+    }
+  };
+
+  const renderTask = ({ item }: { item: Task }) => (
+    <View style={styles.taskContainer}>
+      <Text style={styles.taskId}>{item.taskId}</Text>
+      <Text style={styles.taskAssignedTo}>{item.assignedTo}</Text>
+      <Text style={styles.taskStatus}>{item.status}</Text>
+      <Text style={styles.taskComments}>{item.comments}</Text>
     </View>
   );
 
   return (
-    <View>
-      <Text>Task List</Text>
+    <View style={styles.container}>
       <FlatList
         data={tasks}
-        renderItem={renderTaskItem}
+        renderItem={renderTask}
         keyExtractor={(item) => item.taskId}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  taskContainer: {
+    marginBottom: 16,
+  },
+  taskId: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  taskAssignedTo: {
+    fontSize: 14,
+    color: 'gray',
+  },
+  taskStatus: {
+    fontSize: 14,
+  },
+  taskComments: {
+    fontSize: 14,
+  },
+});
 
 export default TaskList;
